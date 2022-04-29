c0c9a0942d6cf27e88a45030db65183791684f80
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2022-04-29 09:23:38 +0300

    block project

--- assets/script/api.js
+++ assets/script/api.js
@@ -1,9 +1,9 @@
 export default {
     projects: [
         {id: 1, urlCode: '', urlDesign: '', urlProject: '', urlImage: './assets/img/logo.jpg'},
-        {id: 2, urlCode: '', urlDesign: '', urlProject: '', urlImage: ''},
+        {id: 2, urlCode: '', urlDesign: '', urlProject: '', urlImage: './assets/img/logo.jpg'},
         {id: 3, urlCode: '', urlDesign: '', urlProject: '', urlImage: './assets/img/logo.jpg'},
-        {id: 4, urlCode: '', urlDesign: '', urlProject: '', urlImage: ''},
+        {id: 4, urlCode: '', urlDesign: '', urlProject: '', urlImage: './assets/img/back.png'},
         {id: 5, urlCode: '', urlDesign: '', urlProject: '', urlImage: './assets/img/logo.jpg'}
     ]
 }
